c67a02c588c06acd270fe59842a14ed006504295
dyank432 <55112870+dyank432@users.noreply.github.com>
2022-12-05 18:43:42 -0700

    initial commit

---
+++ google_spreadsheet.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+
+import os
+import glob
+import time
+
+import json
+import sys
+import time
+import datetime
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# ==================================================
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+# =================================================
+
+DHT_PIN  = 4
+
+GDOCS_OAUTH_JSON       = 'google-auth.json'
+
+# Google Docs spreadsheet name.
+GDOCS_SPREADSHEET_NAME = 'pi-temperature-log'
+
+# How long to wait (in seconds) between measurements.
+FREQUENCY_SECONDS      = 120
+
+# ================================================
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+
+# ================================================
+
+def login_open_sheet(oauth_key_file, spreadsheet):
+    """Connect to Google Docs spreadsheet and return the first worksheet."""
+    try:
+        scope =  ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(oauth_key_file, scope)
+        gc = gspread.authorize(credentials)
+        worksheet = gc.open(spreadsheet).sheet1
+        return worksheet
+    except Exception as ex:
+        print('Unable to login and get spreadsheet.  Check OAuth credentials, spreadsheet name, and make sure spreadsheet is shared to the client_email address in the OAuth .json file!')
+        print('Google sheet login failed with error:', ex)
+        sys.exit(1)
+
+
+print('Logging sensor measurements to {0} every {1} seconds.'.format(GDOCS_SPREADSHEET_NAME, FREQUENCY_SECONDS))
+print('Press Ctrl-C to quit.')
+
+worksheet = None
+while True:
+    # Login if necessary.
+    if worksheet is None:
+        worksheet = login_open_sheet(GDOCS_OAUTH_JSON, GDOCS_SPREADSHEET_NAME)
+
+    # Attempt to get sensor reading.
+    temp = read_temp()
+
+    # Skip to the next reading if a valid measurement couldn't be taken.
+    # This might happen if the CPU is under a lot of load and the sensor
+    # can't be reliably read (timing is critical to read the sensor).
+    if temp is None:
+        time.sleep(2)
+        continue
+
+    print('Temperature: {0:0.1f} C'.format(temp))
+
+    # Append the data in the spreadsheet, including a timestamp
+    try:
+        #worksheet.append_row((datetime.datetme.now().isoformat(), temp))
+        worksheet.append_row((datetime.datetime.now().strftime('%Y-%m-%d'), datetime.datetime.now().strftime('%H:%M'), temp))
+    except:
+        # Error appending data, most likely because credentials are stale.
+        # Null out the worksheet so a login is performed at the top of the loop.
+        print('Append error, logging in again')
+        worksheet = None
+        time.sleep(FREQUENCY_SECONDS)
+        continue
+
+    # Wait 30 seconds before continuing
+    print('Wrote a row to {0}'.format(GDOCS_SPREADSHEET_NAME))
+    time.sleep(FREQUENCY_SECONDS)
